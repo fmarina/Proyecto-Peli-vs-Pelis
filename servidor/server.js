238c706c281cb9ea9a26a74b12fa621dd136de7d
Marina <marinafaenze@hotmail.com>
2020-05-14 17:03:02 -0300

    import routes to server

--- servidor/server.js
+++ servidor/server.js
@@ -1,12 +1,16 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const cors       = require('cors');
+const routes     = require('./config/routes');
 const app = express();
 const port = 8080;
 
 app.use(cors());
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+routes(app);
 
 app.listen(port, () => {
     console.log(`server is listening on ${port}`)
