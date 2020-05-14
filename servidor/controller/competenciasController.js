3c545affeb77ee6d23d51eb068c9959f21ee5264
Marina <marinafaenze@hotmail.com>
2020-05-14 16:13:59 -0300

    controller folder creater, competenciasController.js file created

---
+++ servidor/controller/competenciasController.js
@@ -0,0 +1,20 @@
+const connection = require('../conexiondb');
+
+const controller = {
+    listarCompetencias : function(req, res) {
+        
+        const sql = "SELECT * FROM competencia";
+        
+        connection.query(sql, function(err, results) {
+            if(err) return res.status(404).send("Hubo un error en la consulta cliente");
+            console.log("results: ", JSON.stringify(results));
+            res.send(JSON.stringify(results));
+        });        
+    },
+
+
+}
+
+
+
+module.exports = controller;
