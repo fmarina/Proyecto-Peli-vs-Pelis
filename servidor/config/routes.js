f71a0946d519cfbd6ab871ac7dcaa44f678f99f1
Marina <marinafaenze@hotmail.com>
2020-05-15 10:08:42 -0300

    endpoint post votar created

--- servidor/config/routes.js
+++ servidor/config/routes.js
@@ -3,4 +3,5 @@ const competenciaController = require('../controller/competenciasController');
 module.exports = function(app){
     app.get('/competencias', competenciaController.listarCompetencias);    
     app.get('/competencias/:id/peliculas', competenciaController.obtenerPeliculasAleatorias);
+    app.post('/competencias/:id/voto', competenciaController.votar);
 }
