76dfaf4624665a96a6c8037b001129f13bc5dd2f
Marina <marinafaenze@hotmail.com>
2020-05-14 21:35:52 -0300

    route to /competencias/:id/peliculas created

--- servidor/config/routes.js
+++ servidor/config/routes.js
@@ -1,5 +1,6 @@
 const competenciaController = require('../controller/competenciasController');
 
 module.exports = function(app){
-    app.get('/competencias', competenciaController.listarCompetencias);
+    app.get('/competencias', competenciaController.listarCompetencias);    
+    app.get('/competencias/:id/peliculas', competenciaController.obtenerPeliculasAleatorias);
 }
