6174f7cb854110c53daa8d38b1ff845b100246fa
Marina <marinafaenze@hotmail.com>
2020-05-15 10:00:50 -0300

    voto table created

--- scripts/competencia.sql
+++ scripts/competencia.sql
@@ -6,4 +6,18 @@ CREATE TABLE `competencia` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(700) NOT NULL,
   PRIMARY KEY (`id`)
-) 
+)
+
+
+DROP TABLE IF EXISTS `voto`;
+
+CREATE TABLE `voto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `competencia_id` INT NOT NULL,
+  `pelicula_id` INT(11) UNSIGNED NOT NULL, 
+  PRIMARY KEY (id),
+  FOREIGN KEY (competencia_id) REFERENCES competencia (id),
+  FOREIGN KEY (pelicula_id) REFERENCES pelicula (id)
+);
+
+
