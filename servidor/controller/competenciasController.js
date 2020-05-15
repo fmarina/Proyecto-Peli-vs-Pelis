9667d9ba9ca251769967fc683cfd830da264109a
Marina <marinafaenze@hotmail.com>
2020-05-15 09:05:25 -0300

    validated if the competition exists

--- servidor/controller/competenciasController.js
+++ servidor/controller/competenciasController.js
@@ -22,9 +22,11 @@ const controller = {
         const competencia_id = req.params.id;
         const sql = "SELECT * FROM competencia WHERE id = " + competencia_id;
 
-        connection.query(sql, function(err, competenciaResult) {
-            
+        connection.query(sql, function(err, competenciaResult) {      
+
             if(err) return res.status(404).send("Hubo un error en la consulta obtenerPeliculasAleatorias");
+            
+            if(competenciaResult.length === 0) return res.status(404).send("Error. La competencia con el identificador enviado no existe.");            
             
             const sql = "SELECT id, poster, titulo FROM pelicula ORDER BY RAND() LIMIT 2;";
 
@@ -33,8 +35,8 @@ const controller = {
                 if(err) return res.status(404).send("Hubo un error en la consulta obtenerPeliculasAleatorias");
                 
                 const result = {
-                    'peliculas'   : peliculaResult,
-                    'competencia' : competenciaResult[0]
+                    'peliculas'   : peliculaResult,    // [{id, poster, titulo}, {id, poster, titulo}]
+                    'competencia' : competenciaResult[0]  // [{id, nombre}]
                 }
 
                 res.send(JSON.stringify(result));
