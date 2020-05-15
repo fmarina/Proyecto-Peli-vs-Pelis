9755520e2bf87c0bafc7f447cf087e3993e66160
Marina <marinafaenze@hotmail.com>
2020-05-15 15:01:32 -0300

    endpoint get obtenerResultados created

--- servidor/config/routes.js
+++ servidor/config/routes.js
@@ -4,4 +4,5 @@ module.exports = function(app){
     app.get('/competencias', competenciaController.listarCompetencias);    
     app.get('/competencias/:id/peliculas', competenciaController.obtenerPeliculasAleatorias);
     app.post('/competencias/:id/voto', competenciaController.votar);
+    app.get("/competencias/:id/resultados", competenciaController.obtenerResultados);
 }
