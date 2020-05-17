a9e81b506e9ae2350d3ab13e61b2ad4ae042d22e
Marina <marinafaenze@hotmail.com>
2020-05-17 12:14:57 -0300

    endpoint get /actores created

--- servidor/config/routes.js
+++ servidor/config/routes.js
@@ -3,11 +3,11 @@ const competenciaController = require('../controller/competenciasController');
 module.exports = function(app){
     app.get('/generos', competenciaController.cargarGeneros);
     app.get('/directores', competenciaController.cargarDirectores);
+    app.get('/actores', competenciaController.cargarActores);
     app.get('/competencias', competenciaController.listarCompetencias);    
     app.get('/competencias/:id/peliculas', competenciaController.obtenerPeliculasAleatorias);
     app.post('/competencias/:id/voto', competenciaController.votar);
     app.get('/competencias/:id/resultados', competenciaController.obtenerResultados);
     app.post('/competencias', competenciaController.crearNuevaCompetencia);
-    app.delete('/competencias/:id/votos', competenciaController.eliminarVotos);
-    
+    app.delete('/competencias/:id/votos', competenciaController.eliminarVotos);    
 }
